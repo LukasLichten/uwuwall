65917ffc81de7f1ba3636b9f6bd0e98bd99d16fa
Lukas Lichten <lichtenrider@hotmail.de>
2023-11-08 12:02:40 +0100

    Fiwxed Message Owdew - Added Sewnding new Messages, but it doews not save *sad uwu*

--- main.go
+++ main.go
@@ -22,77 +22,87 @@ type Page struct {
 	Messages   []Message
 }
 
+type State struct {
+	Database        []Message
+	ElementsPerPage int
+	PageTemlate     *template.Template
+}
+
 func main() {
 	log.Print("Stawting Sewvew")
 
-	var database []Message // Stawte stowage www
-	database = append(database, Message{User: "", Message: "Test", Time: "A long time ago"})
-	database = append(database, Message{User: "", Message: "Test 2", Time: "A long time ago"})
-	database = append(database, Message{User: "", Message: "Test 3", Time: "A long time ago"})
+	var state = State{
+		ElementsPerPage: 2,
+		PageTemlate:     template.Must(template.ParseFiles("templates/page.html")),
+	}
 
-	tmpl := template.Must(template.ParseFiles("templates/page.html"))
+	state.Database = append(state.Database, Message{User: "", Message: "Test", Time: "A long time ago"})
+	state.Database = append(state.Database, Message{User: "", Message: "Test 2", Time: "A long time ago"})
+	state.Database = append(state.Database, Message{User: "", Message: "Test 3", Time: "A long time ago"})
 
 	r := mux.NewRouter()
 
-	elements_per_page := 4
+	r.HandleFunc("/", state.root_page)
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// fiwst page
-		counter := elements_per_page
-		if len(database) < counter {
-			counter = len(database)
-		}
-
-		pageStart := len(database) - counter
-		if pageStart < 0 {
-			pageStart = 0
-		}
-
-		doit := slices.Clone[[]Message](database)[pageStart:len(database)]
-		page := Page{
-			PageNumber: 1,
-			Messages:   doit,
-		}
-		tmpl.Execute(w, page)
-	})
-
-	r.HandleFunc("/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		// Some Pawge
-		vars := mux.Vars(r)
-		pageString := vars["page"]
-
-		pageNum, e := strconv.ParseInt(pageString, 10, 64)
-		pageNumber := int(pageNum)
-
-		counter := elements_per_page
-
-		if e != nil {
-			fmt.Fprint(w, e.Error())
-			return
-		} else if float64(pageNum) > (float64(len(database))/float64(counter) + 0.5) {
-			fmt.Fprint(w, "Thewe whewe wess messages then expewcted...")
-			return
-		} else if pageNumber < 1 {
-			fmt.Fprint(w, "Page must bwe numbewed 1 ow highew")
-			return
-		}
-
-		pageStart := counter * (pageNumber - 1)
-		pageEnd := counter * pageNumber
-
-		if len(database) < pageEnd {
-			pageEnd = len(database)
-		}
-
-		doit := slices.Clone[[]Message](database)[pageStart:pageEnd]
-
-		page := Page{
-			PageNumber: int(pageNumber),
-			Messages:   doit,
-		}
-		tmpl.Execute(w, page)
-	})
+	r.HandleFunc("/page/{page}", state.some_page)
 
 	log.Fatal(http.ListenAndServe(":6969", r))
 
+}
+
+func (state State) root_page(w http.ResponseWriter, r *http.Request) {
+	state.render_page(w, r, 1)
+}
+
+func (state State) some_page(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	pageString := vars["page"]
+
+	pageNum, e := strconv.ParseInt(pageString, 10, 64)
+
+	if e != nil {
+		fmt.Fprint(w, e.Error())
+		return
+	}
+	pageNumber := int(pageNum)
+
+	state.render_page(w, r, pageNumber)
+}
+
+func (state State) render_page(w http.ResponseWriter, r *http.Request, pageNumber int) {
+	log.Print("Page call " + strconv.FormatInt(int64(len(state.Database)), 10))
+
+	if float64(pageNumber) > (float64(len(state.Database))/float64(state.ElementsPerPage) + 0.5) {
+		fmt.Fprint(w, "Thewe whewe wess messages then expewcted...")
+		return
+	} else if pageNumber < 1 {
+		fmt.Fprint(w, "Page must bwe numbewed 1 ow highew")
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		// New Message
+		msg := r.FormValue("new_message")
+		state.Database = append(state.Database, Message{User: "", Message: msg, Time: "A long time ago"})
+		// We continuwe nowmawy
+		// We shouwd maybe wediwect to get, as bwowsew gets confuwsed with ouw fowm
+
+		// Messages awe wost anyway wight now *sad uwu*
+	}
+
+	pageStart := len(state.Database) - state.ElementsPerPage*pageNumber
+	pageEnd := len(state.Database) - state.ElementsPerPage*(pageNumber-1)
+
+	if 0 > pageStart {
+		pageStart = 0
+	}
+
+	doit := slices.Clone[[]Message](state.Database)[pageStart:pageEnd]
+	slices.Reverse[[]Message](doit)
+
+	page := Page{
+		PageNumber: int(pageNumber),
+		Messages:   doit,
+	}
+	state.PageTemlate.Execute(w, page)
 }
